e3b48679ddaec85d543cf3c9f5bd628f3309a120
gstewart9727 <37451434+gstewart9727@users.noreply.github.com>
2020-03-03 13:36:45 -0500

    Performed additional debugging. Moving to administrator account

---
+++ Open3D/Test.py
@@ -0,0 +1,146 @@
+import open3d as o3d
+import numpy as np
+import copy
+import time
+from scipy.spatial import distance
+import scipy
+
+#scipy.spatial.distance.directed_hausdorff
+
+
+def draw_registration_result(source, target, transformation):
+    source_temp = copy.deepcopy(source)
+    target_temp = copy.deepcopy(target)
+    source_temp.paint_uniform_color([1, 0.706, 0])
+    target_temp.paint_uniform_color([0, 0.651, 0.929])
+    source_temp.transform(transformation)
+    o3d.visualization.draw_geometries([source_temp, target_temp])
+
+
+def preprocess_point_cloud(pcd, voxel_size):
+    print(":: Downsample with a voxel size %.3f." % voxel_size)
+    pcd_down = pcd.voxel_down_sample(voxel_size)
+
+    radius_normal = voxel_size * 2
+    print(":: Estimate normal with search radius %.3f." % radius_normal)
+    pcd_down.estimate_normals(
+        o3d.geometry.KDTreeSearchParamHybrid(radius=radius_normal, max_nn=30))
+
+    radius_feature = voxel_size * 5
+    print(":: Compute FPFH feature with search radius %.3f." % radius_feature)
+    pcd_fpfh = o3d.registration.compute_fpfh_feature(
+        pcd_down,
+        o3d.geometry.KDTreeSearchParamHybrid(radius=radius_feature, max_nn=100))
+    return pcd_down, pcd_fpfh
+
+
+def prepare_dataset(voxel_size):
+    print(":: Load two point clouds and disturb initial pose.")
+    source = o3d.io.read_point_cloud("Mesh2.ply")
+    target = o3d.io.read_point_cloud("cleanface.ply")
+    source.scale(0.005)
+    target.scale(10.0)
+    trans_init = np.asarray([[0.0, 0.0, 1.0, 0.0], [1.0, 0.0, 0.0, 0.0],
+                             [0.0, 1.0, 0.0, 0.0], [0.0, 0.0, 0.0, 1.0]])
+    source.transform(trans_init)
+    draw_registration_result(source, target, np.identity(4))
+
+    source_down, source_fpfh = preprocess_point_cloud(source, voxel_size)
+    target_down, target_fpfh = preprocess_point_cloud(target, voxel_size)
+    return source, target, source_down, target_down, source_fpfh, target_fpfh
+
+
+def execute_global_registration(source_down, target_down, source_fpfh,
+                                target_fpfh, voxel_size):
+    distance_threshold = voxel_size * 1.5
+    print(":: RANSAC registration on downsampled point clouds.")
+    print("   Since the downsampling voxel size is %.3f," % voxel_size)
+    print("   we use a liberal distance threshold %.3f." % distance_threshold)
+    result = o3d.registration.registration_ransac_based_on_feature_matching(
+        source_down, target_down, source_fpfh, target_fpfh, distance_threshold,
+        o3d.registration.TransformationEstimationPointToPoint(False), 4, [
+            o3d.registration.CorrespondenceCheckerBasedOnEdgeLength(0.9),
+            o3d.registration.CorrespondenceCheckerBasedOnDistance(
+                distance_threshold)
+        ], o3d.registration.RANSACConvergenceCriteria(4000000, 500))
+    return result
+
+
+def refine_registration(source, target, source_fpfh, target_fpfh, voxel_size):
+    distance_threshold = voxel_size * 0.4
+    print(":: Point-to-plane ICP registration is applied on original point")
+    print("   clouds to refine the alignment. This time we use a strict")
+    print("   distance threshold %.3f." % distance_threshold)
+    result = o3d.registration.registration_icp(
+        source, target, distance_threshold, result_ransac.transformation,
+        o3d.registration.TransformationEstimationPointToPlane())
+    return result
+
+
+def execute_fast_global_registration(source_down, target_down, source_fpfh,
+                                     target_fpfh, voxel_size):
+    distance_threshold = voxel_size * 0.1
+    print(":: Apply fast global registration with distance threshold %.3f" \
+            % distance_threshold)
+    result = o3d.registration.registration_fast_based_on_feature_matching(
+        source_down, target_down, source_fpfh, target_fpfh,
+        o3d.registration.FastGlobalRegistrationOption(
+            maximum_correspondence_distance=distance_threshold))
+    return result
+
+
+if __name__ == "__main__":
+
+    voxel_size = 0.05  # means 5cm for the dataset
+    source, target, source_down, target_down, source_fpfh, target_fpfh = \
+            prepare_dataset(voxel_size)
+
+    start = time.time()
+    result_ransac = execute_global_registration(source_down, target_down,
+                                                source_fpfh, target_fpfh,
+                                                voxel_size)
+    time_match = time.time() - start
+    print("Global registration took %.3f sec.\n" % (time_match))
+    print(result_ransac)
+    draw_registration_result(source_down, target_down,
+                             result_ransac.transformation)
+
+    # start = time.time()
+    # result_fast = execute_fast_global_registration(source_down, target_down,
+    #                                                source_fpfh, target_fpfh,
+    #                                                voxel_size)
+    # print("Fast global registration took %.3f sec.\n" % (time.time() - start))
+    # print(result_fast)
+    # draw_registration_result(source_down, target_down,
+    #                          result_fast.transformation)
+
+    start = time.time()
+    trans_source = copy.deepcopy(source_down)
+    trans_source.transform(result_ransac.transformation)
+
+
+    distances = distance.cdist(np.asarray(trans_source.points), np.asarray(target_down.points), 'euclidean')
+
+    print(distances.shape)
+
+    distances = np.min(np.array(distances), axis=0)
+
+    time_dist = time.time() - start
+
+
+    print("Distance calculations took %.3f sec.\n" % (time_dist))
+
+    trans_source.paint_uniform_color([1, 0.706, 0])
+    target_down.paint_uniform_color([0, 0.651, 0.929])
+
+    a = np.logical_and(distances > 0.08, distances < 0.3)
+
+    color_array = np.asarray(target_down.colors)
+
+    color_array[a, :] = [1, 0, 0]
+
+    target_down.colors = o3d.utility.Vector3dVector(color_array)
+
+    o3d.visualization.draw_geometries([trans_source, target_down])
+
+    print("Total time is %.3f sec.\n" % (time_dist + time_match))
--- PointCloud_Analysis.m
+++ PointCloud_Analysis.m
@@ -3,127 +3,125 @@
 % Programmers : Shreyas Macwan, Gabriel Stewart
 % Description : This file imports two point clouds, then performs Point Cloud Registration using ICP Algorithm 
 
-% Reading the point cloud and storing it into a pointcloud object from a
-% .PLY file 
+% Add realsense wrapper to path
+addpath('C:\Program Files (x86)\Intel RealSense SDK 2.0\matlab\')  
+savepath .\pathdef.m
 
-refMold = pcread('.\CAD_model.ply');        % --- Reading Reference PointCloud PLY file 
-refCamera = pcread('.\Live_model.ply');     % --- Reading Camera PointCloud PLY file
+% Reading the point cloud and storing it into a pointcloud object from a .txt file 
+pointData = load('.\Training Mold\CadData.txt');
+moldCloud  = pointCloud(pointData);
+
+% Collect point cloud from camera
+camCloud = pcread('.\Training Mold\CamData.ply');
+%rawData = collectData();
+%camCloud = pointCloud(rawData);
+
+% Start Timer
+tic 
+
+% Denoise the pointclouds before further processing
+moldCloud_denoise = pcdenoise(moldCloud);
+camCloud_denoise = pcdenoise(camCloud);
 
 % Downsampling both the pointclouds using 'gridAverage' method  
-refMold_down = pcdownsample(refMold,'gridAverage',15);
-refCam_down= pcdownsample(refCamera,'gridAverage',0.010);
+moldCloud_down = pcdownsample(moldCloud_denoise,'gridAverage',8);
+camCloud_down= pcdownsample(camCloud_denoise,'gridAverage',0.008);
 
-% Denoising a pointcloud generated from a RealSense Camera
-refCam_denoise = pcdenoise(refCam_down,'NumNeighbors',8,...
-    'Threshold',0.1);
-
-% For a quality registration, scaling the pointcloud (RealSense Camera)
-% Create matrix for scaling
-A = [2100  0   0       0; ...
-     0	2100   0       0; ...
-     0	0   2100       0; ...
+% Scale the camera cloud data (may be replaced)
+A = [1000  0   0       0; ...
+     0	1000   0       0; ...
+     0	0   1000       0; ...
      0	0   0       1];
  % Apply scale
-tform1 = affine3d(A);
-
-% Scaling and Transformation of Camera pointcloud using the 'pctransform' method 
-ptCloudTformed = pctransform(refCam_denoise,tform1);
+scale = affine3d(A);
+camCloud_scaled = pctransform(camCloud_down,scale);
 
 % Displaying the pointcloud in the 1st Quadrant using 'pcshow' method
-subplot(2,2,1)
-pcshow(ptCloudTformed)
-
-% Rotating reference pointcloud to 90 degrees 
-% Evaluate constant
-n= pi/2;
-% Create matrix for transformation
-A2 =[1  0   0       0; ...
-     0	cos(n)    sin(n)       0; ...
-     0	-sin(n)   cos(n)       0; ...
-     0	0   0       1];
-% Apply transformation
-tform2 = affine3d(A2);
- 
-% Scaling and Transformation of Reference pointcloud using the 'pctransform' method 
-ptCloudTformend_REF = pctransform(refMold_down,tform2);
+subplot(2,2,1);
+pcshow(camCloud_scaled);
 
 % Displaying the pointcloud in the 2nd Quadrant using 'pcshow' method
-subplot(2,2,2)
-pcshow(ptCloudTformend_REF)
+subplot(2,2,2);
+pcshow(moldCloud_down);
  
 %Displaying the pointcloud in the 3rd Quadrant
 %Visualizing both point clouds from above steps, seeing the difference in
 %two using pcshowpair
-subplot(2,2,3)
-pcshowpair(ptCloudTformed,ptCloudTformend_REF, 'MarkerSize', 20);
+subplot(2,2,3);
+pcshowpair(camCloud_scaled,moldCloud_down, 'MarkerSize', 20);
 
-% tic  / toc for see the time required for operation
 % Use of ICP Pointcloud Registration Algorithm which uses Local Registration scheme  
-tic  
-reg_cloud= pcregistericp(ptCloudTformed,ptCloudTformend_REF,'Metric','pointToPoint');
-toc
+%tform = pcregistericp(camCloud_down, moldCloud_down,'Metric','pointToPoint');
+tform = pcregistercpd(camCloud_scaled, moldCloud_down, 'Transform', 'Rigid', 'Tolerance', 1000);
+%tform = pcregisterndt(camCloud_scaled, moldCloud_down, 10);
 
 % Transforming Camera pointcloud with the new transformed Co-ordinates from
 % ICP Algorithm
-ptCloudTransformed = pctransform(ptCloudTformed,reg_cloud);
+camCloud_tformed = pctransform(camCloud_scaled,tform);
+
+% tform = pcregistericp(camCloud_tformed, moldCloud_down, 'Metric', 'pointToPlane');
+% camCloud_tformed_B = pctransform(camCloud_tformed, tform);
 
 %Displaying the pointcloud in the 4th Quadrant
 % Visualizing both point clouds from above steps, seeing the difference in two
 subplot(2,2,4)
-pcshowpair(ptCloudTransformed,ptCloudTformend_REF, 'MarkerSize', 100)
+pcshowpair(camCloud_tformed,moldCloud_down, 'MarkerSize', 100)
 
-%Creates a new instance of the screen
-figure()
+% End Timer
+toc
 
-%ptCloudTransformed --- transformed registered cloud 
-%ptCloudTformend_REF -- Reference mold cloud 
-
-% Getting the XYZ Coordinates point location from both Pointclouds
-xyz_transformed = ptCloudTransformed.Location;
-xyz_ref_mold = ptCloudTformend_REF.Location;
-
-% Finding the Euclidean Distance of Reference pointcloud's point to Icp
-% Transformed PointCloud points using pdist2 method
-all_eu_Distance = pdist2(xyz_transformed,xyz_ref_mold);
-
-% Getting the minimum distance of all the points from the ICP transformed PointCloud  
-min_distance = min(all_eu_Distance,[],2);
-
-% Initializing an empty array to store points location 
-a=[];
-
-% Iterating through all the distances and setting and threshold distance 
-for n=1:size(min_distance,1)
-    % If distance is above threshold
-    if min_distance(n)>12.000 
-        %Points Out of the minimum Threshold value, assigning the color RED
-        %to those points
-        ptCloudTransformed.Color(n,:) = [255 0 0];
-
-        %Storing point's location in the array generated above 
-        a= cat(1,a,ptCloudTransformed.Location(n,:));
-
-        % elseif min_distance(n)<22.000 && min_distance(n)>18.000
-        % ptCloudTransformed.Color(n,:) = [0 0 255];
-
-    else      
-        % Turning Validated Points to color GREEN
-        ptCloudTransformed.Color(n,:) = [0 255 0];
-   end
-end
-
-% Creadting pointcloud only for those points which are out of Minimum Threshold 
-newpoint = pointCloud(a);
-
-% Using 'pcshow' method, Visualizing Transformed pointcloud in the 1st plot of the
-% scrren
-subplot(2,2,1)
-pcshow(ptCloudTransformed);
-
-% Using 'pcshow' method, Visualizing the differentiated pointcloud in the 2nd plot of the
-% scrren
-subplot(2,2,2)
-pcshow(newpoint)
+% %Creates a new instance of the screen
+% figure()
+% 
+% %ptCloudTransformed --- transformed registered cloud 
+% %ptCloudTformend_REF -- Reference mold cloud 
+% 
+% % Getting the XYZ Coordinates point location from both Pointclouds
+% xyz_transformed = camCloud_tformed.Location;
+% xyz_ref_mold = moldCloud_down.Location;
+% 
+% % Finding the Euclidean Distance of Reference pointcloud's point to Icp
+% % Transformed PointCloud points using pdist2 method
+% all_eu_Distance = pdist2(xyz_transformed,xyz_ref_mold);
+% 
+% % Getting the minimum distance of all the points from the ICP transformed PointCloud  
+% min_distance = min(all_eu_Distance,[],2);
+% 
+% % Initializing an empty array to store points location 
+% a=[];
+% 
+% % Iterating through all the distances and setting and threshold distance 
+% for n=1:size(min_distance,1)
+%     % If distance is above threshold
+%     if min_distance(n)>12.000 
+%         %Points Out of the minimum Threshold value, assigning the color RED
+%         %to those points
+%         %ptCloudTransformed.Color(n,:) = [255 0 0];
+% 
+%         %Storing point's location in the array generated above 
+%         a= cat(1,a,ptCloudTransformed.Location(n,:));
+% 
+%         % elseif min_distance(n)<22.000 && min_distance(n)>18.000
+%         % ptCloudTransformed.Color(n,:) = [0 0 255];
+% 
+%     else      
+%         % Turning Validated Points to color GREEN
+%         %ptCloudTransformed.Color(n,:) = [0 255 0];
+%    end
+% end
+% 
+% % Creadting pointcloud only for those points which are out of Minimum Threshold 
+% newpoint = pointCloud(a);
+% 
+% % Using 'pcshow' method, Visualizing Transformed pointcloud in the 1st plot of the
+% % scrren
+% subplot(2,2,1)
+% pcshow(ptCloudTransformed);
+% 
+% % Using 'pcshow' method, Visualizing the differentiated pointcloud in the 2nd plot of the
+% % scrren
+% subplot(2,2,2)
+% pcshow(newpoint)
 
 
 
